199a56f6e1472713517af34b98870d2094c2ac35
Grishma-Atanur <atanur.grishma@gmail.com>
2024-12-04 15:15:48 +0100

    Updated Demo3

--- src/test/java/script/Demo3.java
+++ src/test/java/script/Demo3.java
@@ -1,1 +1,25 @@
-Hi
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+import generic.Utility;
+import page.FBPage;
+
+public class Demo3 extends BaseTest
+{
+
+	@Test
+	public void testDemo1()
+	{
+		String v=Utility.getExcelData(xlpath, "sheet1", 0, 0);
+		Reporter.log("Data from xl:"+v,true);
+		String title=driver.getTitle();
+		Reporter.log(title,true);
+		
+		FBPage p=new FBPage(driver);
+		p.setUN(v);
+	}
+}
+
