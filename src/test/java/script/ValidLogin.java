e20137bb7f40659b2b60ac4cde0f5eb067cdf4f9
AksharaBhanu <javagalbhanu@gmail.com>
2024-12-05 19:11:49 +0530

    Data Driving ValidLoginTest

--- src/test/java/script/ValidLogin.java
+++ src/test/java/script/ValidLogin.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
+import generic.Utility;
 import page.HomePage;
 import page.LoginPage;
 
@@ -13,11 +14,13 @@ public class ValidLogin extends BaseTest
 	@Test(priority = 1,groups= {"login"})
 	public void testValidLogin()
 	{
+		String un=Utility.getExcelData(xlpath, "ValidLogin",1,0);
+		String pw=Utility.getExcelData(xlpath, "ValidLogin",1,1);
 //		1. enter valid un
 		LoginPage loginPage=new LoginPage(driver);
-		loginPage.setUserName("admin");
+		loginPage.setUserName(un);
 //		2. enter valid pw
-		loginPage.setPassword("pointofsale");
+		loginPage.setPassword(pw);
 //		3. click on login
 		loginPage.clickGoButton();
 //		4. home page should be displayed
